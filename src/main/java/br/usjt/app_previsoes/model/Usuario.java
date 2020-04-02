38d3ee8fd0cfd796f7b5c02651f5f17f6762aac3
rammaciel <robertoaugusto00@outlook.com>
2020-04-01 22:58:28 -0300

    first commit

---
+++ src/main/java/br/usjt/app_previsoes/model/Usuario.java
@@ -0,0 +1,34 @@
+package br.usjt.app_previsoes.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario {
+	@Id
+	private Long id;
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private String login;
+	private String senha;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+insert into Previsao(id, diaSemana, tempMin, tempMax, umidade, descricao, latitude, longitude, hora) values (1, 'Domingo', 22.0, 31.9, 57, 'Chuvas.', '50°N', '100°W', '8h28');
+
+insert into Previsao(id, diaSemana, tempMin, tempMax, umidade, descricao, latitude, longitude, hora) values (2, 'Segunda', 22.0, 31.9, 57, 'Chuvas.', '40°N', '80°E', '3h58');
+
+insert into Previsao(id, diaSemana, tempMin, tempMax, umidade, descricao, latitude, longitude, hora) values (3, 'Terça', 22.0, 31.9, 57, 'Chuvas.', '-20°S', '-40°W', '6h42');
+
+insert into Previsao(id, diaSemana, tempMin, tempMax, umidade, descricao, latitude, longitude, hora) values (4, 'Domingo', 22.0, 31.9, 57, 'Chuvas.', '-10°S', '20°E', '3h21');
+
+insert into usuario (id, login, senha) values (1, 'admin', 'admin')
